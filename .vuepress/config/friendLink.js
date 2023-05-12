f470b96737f7bf885957fdd2ad18ab64a075a3bc
wencw <1748994690@qq.com>
2023-05-12 16:08:12 +0800

    fix(friendLink.js): 友情链接头像空白占位。

--- .vuepress/config/friendLink.js
+++ .vuepress/config/friendLink.js
@@ -3,12 +3,15 @@ module.exports = [
     title: "午后南杂",
     desc: "Enjoy when you can, and endure when you must.",
     email: "1156743527@qq.com",
+    // logo: 'https://www.recoluan.com/head.png',
+    logo: '/img/common/logo.png',
     link: "https://www.recoluan.com"
   },
   {
     title: "vuepress-theme-reco",
     desc: "A simple and beautiful vuepress Blog & Doc theme.",
-    logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+    // logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+    logo: '/img/common/logo.png',
     link: "https://vuepress-theme-reco.recoluan.com"
   }
 ]
